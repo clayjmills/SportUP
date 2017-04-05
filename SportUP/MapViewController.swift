f58bbac4d0c650d496c5ddc0a9c493fa986a87c1
Clay Mills <AnimalHouse@Clays-MacBook-Pro.local>
2017-04-05 12:34:02 -0600

    add user location shows up on map

--- SportUP/MapViewController.swift
+++ SportUP/MapViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    // create properties
+    var locationManager: CLLocationManager = CLLocationManager()
+    var currentLocation: CustomAnnotation?
+    var shown: Bool = false
+    
 
+    @IBOutlet weak var sportLabel: UILabel!
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        map.delegate = self
+        map.showsUserLocation = true
+        
 
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let _ = "latitude: \(location.coordinate.latitude) longitude: \(location.coordinate.longitude)"
+        
+            let _ = CustomAnnotation(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude, name: "My location")
+            
+        }
     }
     
 
@@ -34,3 +60,28 @@ class MapViewController: UIViewController {
     */
 
 }
+
+//create a custom class
+class CustomAnnotation: NSObject, MKAnnotation {
+    //properties
+    var latitude: Double
+    var longitude: Double
+    var name: String
+    
+    //memberwise initializer
+    init(latitude: Double, longitude: Double, name: String) {
+        self.latitude = latitude
+        self.longitude = longitude
+        self.name = name
+    }
+    //return the coordinate made by latitude and longitude
+    var coordinate: CLLocationCoordinate2D {
+        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        return coordinate
+    }
+    var title: String? {
+        return name
+    }
+}
+
+
