e61fd52808a70a644a881d88e922abfa3a61092b
clayjmills <claymills5@gmail.com>
2017-05-01 08:29:11 -0600

    added icon logo, working on buttons

--- SportUP/MainViewController.swift
+++ SportUP/MainViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    @IBOutlet weak var findPickupGameButtonTapped: UIButton!
+    
+    @IBOutlet weak var createPickupGameButtonTapped: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.updateViews()
 
+    }
+    
+    func updateViews() {
+        
+        self.findPickupGameButtonTapped.backgroundColor = UIColor.lightGray
+        self.findPickupGameButtonTapped.layer.cornerRadius = 16
+        
+        self.createPickupGameButtonTapped.backgroundColor = UIColor.lightGray
+        self.createPickupGameButtonTapped.layer.cornerRadius = 16 
+        
+        
     }
 
 }
